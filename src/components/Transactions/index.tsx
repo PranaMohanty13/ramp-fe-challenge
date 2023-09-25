b144dd73941f1282b6c7ca7777fd269c007ae9d4
Prana Mohanty <94722016+PranaMohanty13@users.noreply.github.com>
2023-09-25 19:42:32 +0000

    Bug 7 solution

--- src/components/Transactions/index.tsx
+++ src/components/Transactions/index.tsx
@@ -1,11 +1,16 @@
-import { useCallback } from "react"
+import { useState, useEffect, useCallback } from "react"
 import { useCustomFetch } from "src/hooks/useCustomFetch"
 import { SetTransactionApprovalParams } from "src/utils/types"
 import { TransactionPane } from "./TransactionPane"
 import { SetTransactionApprovalFunction, TransactionsComponent } from "./types"
 
-export const Transactions: TransactionsComponent = ({ transactions }) => {
+export const Transactions: TransactionsComponent = ({ transactions: initialTransactions }) => {
+  const [transactions, setTransactions] = useState(initialTransactions)
   const { fetchWithoutCache, loading } = useCustomFetch()
+
+  useEffect(() => {
+    setTransactions(initialTransactions)
+  }, [initialTransactions])
 
   const setTransactionApproval = useCallback<SetTransactionApprovalFunction>(
     async ({ transactionId, newValue }) => {
@@ -13,6 +18,14 @@ export const Transactions: TransactionsComponent = ({ transactions }) => {
         transactionId,
         value: newValue,
       })
+
+      setTransactions((prevTransactions) =>
+        prevTransactions
+          ? prevTransactions.map((transaction) =>
+              transaction.id === transactionId ? { ...transaction, approved: newValue } : transaction
+            )
+          : null
+      )
     },
     [fetchWithoutCache]
   )
